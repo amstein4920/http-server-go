8aa1b07a512352cb7e1d47c4f9dfc41b8dcc0619
amstein4920 <amstein4920@gmail.com>
2025-02-19 21:11:42 +0800

    Can receive connections and respond with a 200 status

--- app/server.go
+++ app/server.go
@@ -21,10 +21,28 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	readBuffer := make([]byte, 1024)
+	_, err := conn.Read(readBuffer)
+	if err != nil {
+		fmt.Println("Unable to read connection input")
+		return
+	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
